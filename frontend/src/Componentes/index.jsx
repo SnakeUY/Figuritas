8bcc17a4831d7ef17bf7607b59542a3ea20a363d
SnakeUY <mt2truekill@gmail.com>
2022-11-09 13:17:49 -0300

    Actualizacion2

--- frontend/src/Componentes/index.jsx
+++ frontend/src/Componentes/index.jsx
@@ -4,28 +4,11 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { getFiguritas, getPaises } from "../services/httpConsumer";
 import { Categoria as ModeloCategoria } from "../Modelo/categoria";
-import { Figurita as ModeloFigurita } from "../Modelo/figurita";
-import Figurita from "./figurita";
 import {Componente} from "./categorias";
 const Album = ({ props }) => {
   const [categoria, SetCategoria] = useState([]);
-  const [figurita, setFigurita] = useState([]);
   const [isLoading, setIsLoading] = useState(true)
-  const getCategoria = () =>
-    getPaises(
-      (data) => {
-        SetCategoria(data.map(adaptModelToCountry));
-      },
-      () => console.log("buscando")
-    );
   const adaptModelToCountry = (pais) => new ModeloCategoria(pais.pais);
-  const adaptModelToFigurita = (figurita) =>
-    new ModeloFigurita(
-      figurita.id,
-      figurita.nombre,
-      figurita.pais,
-      figurita.tengo
-    );
 
   useEffect(() => {
      getPaises(
@@ -34,32 +17,8 @@ const Album = ({ props }) => {
       },
       () => setIsLoading(false)
     );
-
-    getFiguritas(
-      (data) => {
-        setFigurita(data.map(adaptModelToFigurita));
-      },
-      () => setIsLoading(false)
-    );
-    /*
-    getCategoria();
-    categoria.forEach((categoria) => {
-      console.log(categoria);
-    });
-    getFigurita();
-    figurita.forEach((figurita) => {
-      console.log(figurita);
-    });*/ 
   }, []);
-  /*
-  const getFigurita = () =>
-    getFiguritas(
-      (data) => {
-        setFigurita(data.map(adaptModelToFigurita));
-      },
-      () => console.log("buscando 2")
-    );
-*/
+
   return (
     <>
       <div className="categorias">
